882222021d5c20cfe5d2a8f4590b0428042b1265
Drong-Yang <100173485+Drong-Yang@users.noreply.github.com>
2024-02-28 10:00:00 +0800

    feat: implement custom DogBreedDataset loader

---
+++ preprocess.py
@@ -0,0 +1,20 @@
+"""Data preprocessing module for dog breed classification."""
+import os
+import cv2
+import pandas as pd
+from torch.utils.data import Dataset, DataLoader
+
+class DogBreedDataset(Dataset):
+    """Custom dataset for dog breed images."""
+    def __init__(self, csv_file, img_dir, transform=None):
+        self.labels = pd.read_csv(csv_file)
+        self.img_dir = img_dir
+        self.transform = transform
+    def __len__(self):
+        return len(self.labels)
+    def __getitem__(self, idx):
+        img_path = os.path.join(self.img_dir, self.labels.iloc[idx, 0])
+        image = cv2.imread(img_path)
+        if self.transform:
+            image = self.transform(image)
+        return image, self.labels.iloc[idx, 1]
